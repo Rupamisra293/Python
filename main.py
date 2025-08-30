f81d3054934eee379403b5f0f62c0de3890b5f3b
Rupamisra293 <Rupamisra293@gmail.com>
2025-08-30 21:12:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-height = float(input("Enter your height in cm: "))
-weight = float(input("Enter your weight in kg: "))
 
-BMI = weight / (height/100)**2
 
-print("Your BMI is", BMI)
 
-if BMI <= 18.4:
-    print("You are underweight.")
-elif BMI <= 24.9:
-    print("You are healthy.")
-elif BMI <= 29.9:
-    print("You are over weight.")
-elif BMI <= 34.9:
-    print("You are severely over weight.")
-elif BMI <= 39.9:
-    print("You are obese.")
+
+# Default function to implement conditions to check leap year  
+def CheckLeap(Year):  
+  # Checking if the given year is leap year  
+  if((Year % 400 == 0) or  
+     (Year % 100 != 0) and  
+     (Year % 4 == 0)):   
+    print("Given Year is a leap Year");  
+  # Else it is not a leap year  
+  else:  
+    print ("Given Year is not a leap Year")  
+
+# Taking an input year from user  
+Year = int(input("Enter the number: "))  
+# Printing result  
+CheckLeap(Year)  
+
+
+#Homework
+#========
+print("Enter a Character: ", end="")
+c = input()
+if len(c)>1:
+    print("\nInvalid Input!")
 else:
-    print("You are severely obese.")
+    if c>='a' and c<='z':
+        print("\n\"" +c+ "\" is an alphabet.")
+    elif c>='A' and c<='Z':
+        print("\n\"" +c+ "\" is an alphabet.")
+    else:
+        print("\n\"" +c+ "\" is not an alphabet!")
