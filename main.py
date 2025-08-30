ef5115687d19ad62c803d9ed3ea981652ff01945
Rupamisra293 <Rupamisra293@gmail.com>
2025-08-30 21:02:02 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-#creating variables
-a = 5
-b = 10
-c = a + b
-print(c)
-# Output: 15
-d = b - a
-print(d)
-# Output: 5
+import keyword
 
-print("Sum of", a, "and", b, "is", c)
+print("Python Keywords:")
+print(keyword.kwlist)
 
-print("Difference of", b, "and", a, "is", d)
-
-name = "Codingal"
-greeting = "Hello, " + name + "!"
-print(greeting)
-
-name = input("Enter your name: ")
-print("Hello, " + name + "! Welcome to Codingal.")
-age = int(input("Enter your age: "))
-next_year_age = age + 1
-print("Next year, you will be", next_year_age, "years old.")
 
 
 
