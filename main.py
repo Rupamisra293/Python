4a5b43b4dc3988c8bb7cc088e5b1d4fd39d65193
Rupamisra293 <Rupamisra293@gmail.com>
2025-08-30 21:00:22 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,89 +1,29 @@
-name = "Codingal"
-age = 2
-weight = 12.5
-isstudent = True
-print("Name:", name)
-print("Age:", age)
-print("Weight:", weight)
-print("Is Student:", isstudent)
-print("Data type of 'name':", type(name))
-print("Data type of 'age':", type(age))
-print("Data type of 'weight':", type(weight))
-print("Data type of 'isstudent':", type(isstudent))
-print("Hello, my name is", name + ". I am", age, "years old and weigh", weight, "kg.")
-# This program defines variables of different data types and prints their values and types.
-# It also demonstrates string concatenation and formatted output.
-age = float(age)
-weight = int(weight)
-print("Converted Age (float):", age)
-print("Converted Weight (int):", weight)
-# This program defines variables of different data types and prints their values and types.
-# It also demonstrates string concatenation and formatted output.
+print("Hello World")
+print("\n")
+print("Goodbye!\nSee you later!")
+print("Have a great day!")
+print('Hi! How are you?')
+print('''This is a multi-line
+print statement.''')
+print('''This is another multi-line
+print statement.''')
 
-x = 5
-y = 10
-sum = x + y
-print("Sum of", x, "and", y, "is:", sum)
-# This program defines variables of different data types and prints their values and types.
-# It also demonstrates string concatenation and formatted output.
-subtraction = y - x
-print("Subtraction of", y, "and", x, "is:", subtraction)
-multiplication = x * y
-print("Multiplication of", x, "and", y, "is:", multiplication)
-division = y / x
-print("Division of", y, "by", x, "is:", division)
-modulus = y % x
-print("Modulus of", y, "and", x, "is:", modulus)
-exponentiation = x ** 2
-print("Exponentiation of", x, "to the power of 2 is:", exponentiation)
-floor_division = y // x
-print("Floor Division of", y, "by", x, "is:", floor_division)
+print(5)
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(16 / 2)
+print(5 ** 2)   
+print(10 % 3)
+print(10 // 3)  
 
-print(x==y)  # False
-print(x!=y)  # True
-print(x>y)   # False
-print(x<y)   # True
-print(x>=y)  # False
-print(x<=y)  # True
-print(x is y)  # False
-print(x is not y)  # True
-print(x in [5, 10, 15])  # True
-print(y not in [5, 10, 15])  # False
-print((x < y) and (x != y))  # True
-print((x > y) or (x == 5))  # True
-print(not(x < y))  # False
-# This program defines variables of different data types and prints their values and types.
+print("Hello" + " " + "World")
+print("Hello" * 3)
+print("Repeat! " * 5)
+print("Hello" + "!" * 3)
+print("Ha" * 4 + "!")
 
-name = "Codingal"
-name1 = 'Codingal'
-name2 = """Codingal"""
-name3 = '''Codingal
-           Home of Coding'''
-print(name, name1, name2, name3)
-print(name[0])  # C
-print(name[1])  # o
-print(name[2])  # d
-print(name[5:8])  # ing
-print(name[:6])  # Coding
-print(name[3:])  # ingal
-print(name[-1])  # l
-print(name[-3:])  # gal
-print(name[::2])  # Cdna
-print(name[::-1])  # laginoC
-print(len(name))  # 8
-print(name.upper())  # CODINGAL
-print(name.lower())  # codingal
-print(name.capitalize())  # Codingal
-print(name.title())  # Codingal
-print(name.strip())  # Codingal
-print(name.replace("Coding", "Code"))  # Codegal
-print(name.split("i"))  # ['Cod', 'ngal']
-print("Coding" in name)  # True
-print("code" not in name)  # True
-# This program defines variables of different data types and prints their values and types.
-# It also demonstrates string concatenation and formatted output.
+print("welcome to", end="**")
 
-fname = "hi"
-print(name + " " + fname)  # Codingal hi
 
 
