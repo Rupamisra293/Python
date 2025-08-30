45fef40ebf75a0f9360e909cd08c7f6186ee02db
Rupamisra293 <Rupamisra293@gmail.com>
2025-08-30 21:06:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-# Storing Values
-tree1 = 98
-tree2 = 94
-tree3 = 41
-tree4 = 95
-tree5 = 11
+# Taking total amount as input from user
+Amount =int(input("Please Enter Amount for Withdraw :"))
 
-# Finding the total of trees
-sum = tree1+tree2+tree3+tree4+tree5
-print("the sum of all the 5 trees is: ", sum)
+# Calculating the number of notes of different denominations
+note_1 = Amount//100
+note_2 = (Amount%100)//50
+note_3 = ((Amount%100)%50)//10
 
-# Finding the average of trees
-average = sum/5
-print("the average of all the tree is :", average)
+
+print( "notes of 100 rupee" , note_1)
+print("notes of 50 rupee" , note_2)
+print("notes of 10 rupee" , note_3)
