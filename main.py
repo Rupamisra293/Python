602fb758c1365629572906ef2861dd407ba0df18
Rupamisra293 <Rupamisra293@gmail.com>
2025-08-30 21:04:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-# Assigning Different Variables
-name = "Penguin"
-age = 15
-is_student = True
-weight = 38.5
+#input a word
+text = str(input("Enter a string: "))
 
-# Printing Different Variables and their Data Type
-print("Name :", name)
-print("Data Type of Name is", type(name))
+# Reverse String 
+# using step value as -1 to iterate in reverse
+revText = text[::-1] 
+text = revText
 
-print("Age :", age)
-print("Data Type of Age is", type(age))
-
-print("is_student :", is_student)
-print("Data Type of is_student is", type(is_student))
-
-print("Weight :", weight)
-print("Data Type of weight is", type(weight))
-
-# Type casting to convert the datatype of variables
-print("\n After Type Casting....")
-age = str(age)
-print(age)
-print("Data Type of age is", type(age))
-weight = int(weight)
-print(weight)
-print("Data Type of Weight is", type(weight))
+print("Reverse of Given String is:")
+print(text)
