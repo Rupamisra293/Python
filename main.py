327361b807b1626db2c197b67a7d3e9a3e2b2a43
Rupa Misra <Rupamisra293@gmail.com>
2025-09-03 21:06:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import keyword
+#Take input for the student that he can attend the exam or not
+medical_cause=input("did you have a medical cause Y or N: ")
+#Take input of the attendance
+atten = int(input("enter the attendance of the student: "))
 
-print("following are the keywords of python")
-print(keyword.kwlist)
+#checking the user input predicting output accordingly
+
+if medical_cause == 'Y': #checking the condition 1
+  print ("You are allowed")
+else:
+  if atten>=75:  #checking the condition 2
+    print ("Allowed")
+  else:
+    print ("Not allowed")
