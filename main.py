0cdc35e4ac7e6ca198ed4b711918876788604bc8
Rupa Misra <Rupamisra293@gmail.com>
2025-09-03 21:11:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-# Take input of number of units consumed from the user
-units = int(input(" Please enter Number of Units you Consumed : "))
+print("Select your ride: ")
+print("1. Bike")
+print("2. Car")
+#take input of number 1 or 2
+#select your ride
+choice = int( input("Enter your choice: ") )
+#User entering option 1 
+if( choice == 1 ): #condition 1 outer if statement
+  print( "what type of bike? " )
+  print("1.Scooty\n")
+  print("2.Scooter\n")
 
-# Check conditions of units consumed 
-# Then calculate amount and surcharge accordingly
-# surcharge is the tax value
+  #Condition for selecting the type of bike
+  choice2=int(input("Enter you choice2: "))
+  if choice2==1: #inner if statement
+    print("you have selected scooty")
+  else:
+    print("you have selected scooter")
 
-# Check for units less than 50
-if(units < 50):
-    amount = units * 2.60 
-    surcharge = 25 
+#User entering option 2
+elif( choice == 2 ): #outer elif statement
+  print( "what type of car?" )
+  print("1.Sedan")
+  print("2.XUV")
+  choice3=int(input("enter your choice3: "))
+  if choice3==1: #inner if statement
+  #condition for selecting the type of car
+    print("you have selected sedan")
+  else:
+    print("you have selected XUV")
 
-# Check for units less than 100
-elif(units <= 100):
-    amount = 130 + ((units - 50) * 3.25)
-    surcharge = 35
-
-# Check for units less than or equal to 200
-elif(units <= 200):
-    amount = 130 + 162.50 + ((units - 100) * 5.26)
-    surcharge = 45
-
-# Check for all the cases other than mentioned 
-# When units consumed are more than 200
-else:
-    amount = 130 + 162.50 + 526 + ((units - 200) * 8.45)
-    surcharge = 75
-
-# Calculate and Display the total electricity bill
-# total amount = amount + surcharge
-total = amount + surcharge
-print("\nElectricity Bill = %.2f"  %total)
+else: #outer else statement
+  print("Wrong choice!")
