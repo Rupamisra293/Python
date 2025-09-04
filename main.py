d02b814ae88aa730039add5d560a095a51e282cc
Rupa Misra <Rupamisra293@gmail.com>
2025-09-04 17:27:51 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-#Take input of a word
-string = input("Please enter your own word : ")
-#take input of a character
-char = input("Please enter your own Character : ")
-i = 0
-count = 0
-#loop will to find the occurence of character
-while(i < len(string)): #string operation
+#take two input from user
+lower = int(input("Enter a lower range: "))
+upper = int(input("Enter a upper range: "))
 
-    if(string[i] == char): #condition 1
-        count = count + 1
-    i = i + 1
-
-#Display the result
-print("The total Number of Times ", char, " has Occurred = " , count)
+print("Prime numbers between", lower, "and", upper, "are:")
+#iterate loop from lower limit to upper limit
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
