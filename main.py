7c1257b8ce9d57531b493d498d9d9d4751a8c3ca
Rupa Misra <Rupamisra293@gmail.com>
2025-09-04 17:25:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-#Take input from user
-num = int(input("Enter a number: "))
-rev = 0
-#checking each digit are the same backward and forward
-temp = num
-while temp>0:
-    rem = temp%10
-    rev = rem + (rev*10)
-    temp = int(temp/10)
-#display the result
-if rev==num:
-    print("\nIt is a Palindrome Number")
-else:
-    print("\nIt is not a Palindrome Number")
+num = int(input("Enter any number\n"))
+
+
+count = 0
+
+while num != 0:
+  count+=1
+  num=num//10
+
+print(count)
