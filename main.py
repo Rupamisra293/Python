6a698fd214856b180ac3b363901c10ed2cebcdbf
Rupa Misra <Rupamisra293@gmail.com>
2025-09-04 17:20:46 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-#Take input of a string
-str1 = input("Please Enter a sentence: ")
-total = 1 #initialise
-
-for i in range(len(str1)): 
-#loop will run to calculate the length using string operation
-    if(str1[i] == ' ' ):
-    #condition 1
-        total = total + 1
-
-print("Total Number of Words in this String = ", total)
+import math
+number=int(input("tell a no"))
+power=int(input("enter the power no"))
+print(math.pow(number,power))
