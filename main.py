3409ce0c637d10f8b28b612aa632aedaa57824ed
Rupa Misra <Rupamisra293@gmail.com>
2025-09-04 17:21:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import math
-number=int(input("tell a no"))
-power=int(input("enter the power no"))
-print(math.pow(number,power))
+#Input the value of terms
+n = int(input("Enter the value of terms: "))
+
+sum = 0  #initialise
+i = 1  #initialise
+while i<=n: #loop will run from 1 to n
+  sum = sum+i
+  i = i+1
+
+print("\nSum =", sum)
