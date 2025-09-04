1d48d51c0ddfec18f1efc67f08086bc5ef3c136f
Rupa Misra <Rupamisra293@gmail.com>
2025-09-04 17:09:51 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-num = int(input("Enter any number\n"))
+#take input from user
+text = input(("Enter a sentence: "))
+text = text.split()
+bigWordLen = 0 #initialise
 
+for wrd in text: #iterate loop
+  wrdLen = len(wrd) #string operation
+  if wrdLen>bigWordLen: #condition 1
+    bigWordLen = wrdLen
 
-count = 0
-
-while num != 0:
-  count+=1
-  num=num//10
-
-print(count)
+print("\nLargest Word: ")
+for wrd in text: 
+  wrdLen = len(wrd)
+  if wrdLen==bigWordLen: #condition 2
+    print(wrd) #display result
