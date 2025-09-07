e855a80fa80c6743b8ed173f7737aaa765b468d1
Rupa Misra <Rupamisra293@gmail.com>
2025-09-07 20:14:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-import turtle    #importing library
-turtle.Screen().bgcolor("orange")
-turtle.Screen().setup(300,400)
-polygon = turtle.Turtle() #defined variable
+import turtle
+
+turtle.Screen().bgcolor("Aqua")
+board = turtle.Turtle()
  
-num_sides = 6 #variable
-side_length = 70
-angle = 360.0 / num_sides
-#iterate loop for total number of side
-for i in range(num_sides):
-    polygon.forward(side_length)
-    polygon.right(angle)
-     
+# first triangle for star
+board.forward(100) # draw base
+ 
+board.left(120)
+board.forward(100)
+ 
+board.left(120)
+board.forward(100)
+ 
+board.penup()
+board.right(150)
+board.forward(50)
+ 
+# second triangle for star
+board.pendown()
+board.right(90)
+board.forward(100)
+ 
+board.right(120)
+board.forward(100)
+ 
+board.right(120)
+board.forward(100)
+ 
 turtle.done()
