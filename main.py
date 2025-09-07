083116f018538ed47a305ced47b9e476df2c540d
Rupa Misra <Rupamisra293@gmail.com>
2025-09-07 20:11:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
-#take input from user
-rowSize = int(input("enter the number of rows: "))
-if rowSize%2==0: #conditions
-  halfDiamRow = int(rowSize/2)
-else:
-  halfDiamRow = int(rowSize/2)+1
-space = halfDiamRow-1
-#loop for upper part 
-for i in range(1, halfDiamRow+1): #loop for rows
-  for j in range(1, space+1): #loop for columns
+rowNum = 5 #initialised number of rows 5
+space = rowNum-1
+
+for i in range(1, rowNum+1): #loops for rows
+  for j in range(1, space+1): #loop for space 
     print(end=" ")
   space = space-1
-  num = 1
-  for j in range(2*i-1):
-    print(end=str(num))
-  #incerementing number at each column
-    num = num+1
+  
+  for j in range(2*i-1): #loop for upper part of diamond
+    print(end="*")
   print()
 space = 1
-#loop for lower part
-for i in range(1, halfDiamRow): #loop for rows
-  for j in range(1, space+1):  #loop for columns
+#loops for lower part of diamond
+for i in range(1, rowNum):
+  for j in range(1, space+1):
     print(end=" ")
   space = space+1
-  num = 1
-  for j in range(1, 2*(halfDiamRow-i)):
-    print(end=str(num)) #display result
-  #incerementing number at each column
-    num = num+1
+  for j in range(1, 2*(rowNum-i)): 
+    print(end="*")
   print()
