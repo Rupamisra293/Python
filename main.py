148239f249af64c62f185557df6753dded94b09c
Rupa Misra <Rupamisra293@gmail.com>
2025-09-10 19:35:17 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-for numb in range(1,5): #example 1 
-  if numb==2:
-  #using pass function
-    pass
-  else:
-    print ("Present Number =  {} ".format(numb))
+price = 2.50
+#define a function to calculate the difference between the amount given and price mentioned
+def calculate_change(amount_given):
+	return amount_given - price
+
+c = calculate_change(4.00)
+print("Change the customer is due", c)
