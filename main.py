694da21607968d984f51cf2f3fb3c275291dac37
Rupa Misra <Rupamisra293@gmail.com>
2025-09-10 19:23:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-def DecimalToBinary(num):
-    if num >= 1:
-        DecimalToBinary(num // 2)
-    print(num % 2, end='')
+import math
 
-# Adding a newline after each binary conversion for clarity
-print("Binary of 5:", end=' ')
-DecimalToBinary(5)
-print()  # Newline
+radius = float (input("enter the radius of a circle:"))
 
-print("Binary of 23:", end=' ')
-DecimalToBinary(23)
-print()  # Newline
-
-print("Binary of 5:", end=' ')
-DecimalToBinary(5)
-print()  # Newline
+circumference= 2*math.pi* radius
+print("circumference of a circle is :%.2f "% circumference)
